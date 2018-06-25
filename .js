cd433304641af0461e39e998aca5933f3e18ea39
IvoryKim107 <30363069+IvoryKim107@users.noreply.github.com>
2018-06-25 17:44:36 -0400

    new site

---
+++ .js
@@ -0,0 +1,21 @@
+// Blackjack by Ivory Simmons
+let deck = 52 Cards
+let cards = 
+  'Ace'
+  'King'
+  'Queen'
+  'Jack'
+  'Ten'
+  'nine'
+  'eight'
+  'seven'
+  'six'
+  'five'
+  'four'
+  'three'
+  'two'
+ let suits = "Hearts", "Diamonds", "Clubs", "Spades", 
+
+];
+Console.log("Cards are delt: ");
+Console.log(" " +)
